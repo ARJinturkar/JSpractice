02834e3c3d0e0713dc7ecfc0f0d5a09302ecfad1
ARJinturkar <jinturkarakash20@gmail.com>
2021-11-26 22:27:34 +0530

    condional

---
+++ conditinal.js
@@ -0,0 +1,25 @@
+var islogin = "akash"
+
+if (islogin == "akash") {
+    console.log("you are logged in");
+} else {
+    console.log("you cant log in");
+}
+
+
+var user = 8.6;
+switch(user)
+{
+    case 8.9:
+        console.log("testprep");
+        break
+    case 8.6:
+        console.log("admin");
+        break;
+    default:
+       console.log("default");
+        break;
+    case 76.8:
+        console.log("ghj");
+        break;    
+    }
